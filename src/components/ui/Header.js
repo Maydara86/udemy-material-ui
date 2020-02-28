e04d72eeec4650bb666478d2c7933e0c1a8d1784
Mohamed Cherif <cherif.mohamed86@gmail.com>
2020-02-28 13:09:13 +0100

    17. Styling - Themes (Typography)

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
 
 function ElevationScroll(props) {
   const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
-    target: window ? window() : undefined,
   });
 
   return React.cloneElement(children, {
@@ -23,7 +20,11 @@ export default function Header() {
   return (
     <ElevationScroll>
       <AppBar position="fixed">
-        <Toolbar>Arc Development</Toolbar>
+        <Toolbar>
+          <Typography variant="h3">
+            Arc Development
+          </Typography>
+        </Toolbar>
       </AppBar>
     </ElevationScroll>
   )
