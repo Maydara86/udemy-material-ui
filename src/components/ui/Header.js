c2ee6e92925dce27e923bfc856881463830f224f
Mohamed Cherif <cherif.mohamed86@gmail.com>
2020-03-07 00:09:13 +0100

    22. Using Tabs

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,8 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import makeStyles from '@material-ui/styles/makeStyles'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 
 import logo from '../../assets/logo.svg'
 
@@ -27,6 +29,14 @@ const useStyles = makeStyles((theme) => ({
   logo: {
     height: '7em',
   },
+  tabContainer: {
+    marginLeft: 'auto',
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px',
+  },
 }))
 
 export default function Header() {
@@ -39,6 +49,13 @@ export default function Header() {
           {/* disableGutters = remove the side gaps in the toolbar edges */}
           <Toolbar disableGutters>
             <img className={classes.logo} alt="Company Logo" src={logo} />
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="The Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
