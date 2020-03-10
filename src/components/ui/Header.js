d6943ca0543d792cbae4ce76368aeb5e6faa23f4
Mohamed Cherif <cherif.mohamed86@gmail.com>
2020-03-10 23:01:58 +0100

    27. Active Tab Refresh Fix

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import makeStyles from '@material-ui/styles/makeStyles'
@@ -29,7 +29,13 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '3em',
   },
   logo: {
-    height: '7em',
+    height: '8em',
+  },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
   },
   tabContainer: {
     marginLeft: 'auto',
@@ -52,6 +58,21 @@ export default function Header() {
   const classes = useStyles()
   const [value, setValue] = useState(0)
   const handleChange = (e, value) => setValue(value)
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0)
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1)
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2)
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3)
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4)
+    } else if (window.location.pathname === '/estimate' && value !== 5) {
+      setValue(5)
+    }
+  }, [value])
 
   return (
     <>
@@ -59,7 +80,15 @@ export default function Header() {
         <AppBar position="fixed">
           {/* disableGutters = remove the side gaps in the toolbar edges */}
           <Toolbar disableGutters>
-            <img className={classes.logo} alt="Company Logo" src={logo} />
+            <Button
+              component={Link}
+              to="/"
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+              disableRipple
+            >
+              <img className={classes.logo} alt="Company Logo" src={logo} />
+            </Button>
             <Tabs
               className={classes.tabContainer}
               value={value}
